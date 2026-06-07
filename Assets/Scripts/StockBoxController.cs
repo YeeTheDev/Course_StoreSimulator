06cf16ce513bfb36efd5a180aae601e8354e4203
Yee <yee.devindie@gmail.com>
2026-06-07 11:38:49 -0600

    41.Filling The Box

--- Assets/Scripts/StockBoxController.cs
+++ Assets/Scripts/StockBoxController.cs
@@ -1,11 +1,16 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StockBoxController : MonoBehaviour
 {
+    public StockInfo info;
+
     public List<Transform> bigDrinkPoints;
     public List<Transform> cerealPoints, tubeChipsPoints, fruitPoints, largeFruitPoints;
+
+    public List<StockObject> stockInBox;
+
+    public bool testFill;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +21,64 @@ public class StockBoxController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (testFill == true)
+        {
+            testFill = false;
+            SetupBox(info);
+        }
+    }
+
+    public void SetupBox(StockInfo stockType)
+    {
+        info = stockType;
+
+        List<Transform> activePoints = new List<Transform>();
+
+        switch (info.typeOfStock)
+        {
+            case StockInfo.StockType.bigDrink:
+
+                activePoints.AddRange(bigDrinkPoints);
+
+                break;
+
+            case StockInfo.StockType.cereal:
+
+                activePoints.AddRange(cerealPoints);
+
+                break;
+
+            case StockInfo.StockType.chipsTube:
+
+                activePoints.AddRange(tubeChipsPoints);
+
+                break;
+
+            case StockInfo.StockType.fruit:
+
+                activePoints.AddRange(fruitPoints);
+
+                break;
+
+            case StockInfo.StockType.fruitLarge:
+
+                activePoints.AddRange(largeFruitPoints);
+
+                break;
+        }
+
+        if (stockInBox.Count == 0)
+        {
+            for (int i = 0; i < activePoints.Count; i++)
+            {
+                StockObject stock = Instantiate(stockType.stockObject, activePoints[i]);
+                stock.transform.localPosition = Vector3.zero;
+                stock.transform.localRotation = Quaternion.identity;
+
+                stockInBox.Add(stock);
+
+                stock.PlaceInBox();
+            }
+        }
     }
 }
